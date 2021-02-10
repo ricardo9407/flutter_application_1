a7d9acfae0aaa87e8addf6ce885c78bc73f97f3c
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-02-10 14:45:39 -0300

    Adición e implementación de la fuente Lato

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -36,7 +36,8 @@ class DescriptionPlace extends StatelessWidget {
         margin: EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0),
         child: Text(
           namePlace,
-          style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+          style: TextStyle(
+              fontFamily: "Lato", fontSize: 30.0, fontWeight: FontWeight.w900),
           textAlign: TextAlign.left,
         ),
       ),
@@ -49,6 +50,7 @@ class DescriptionPlace extends StatelessWidget {
       margin: new EdgeInsets.only(top: 20.0, right: 20.0, left: 20.0),
       child: Text(descriptionPlace,
           style: const TextStyle(
+              fontFamily: "Lato",
               fontSize: 16.0,
               fontWeight: FontWeight.bold,
               color: Color(0xFF56575a))),
