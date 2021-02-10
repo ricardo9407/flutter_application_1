7695cd4ca0b847bba49c2b1d0b78fc6171a2b858
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-02-10 14:00:56 -0300

    Agregado el constructor de DescriptionPlace

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -5,6 +5,8 @@ class DescriptionPlace extends StatelessWidget {
   int stars;
   String descriptionPlace;
 
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
   String descriptionDummy =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit nec elit ac placerat. Nam et sapien in justo posuere cursus. Maecenas sit amet pretium urna, a tincidunt mi. Morbi ac eros sed purus blandit tincidunt sodales in mi. Proin nec gravida ex. Donec iaculis dapibus nibh, ac tempus justo. Vivamus condimentum nisi in tellus efficitur, ac consequat metus lobortis. Duis ullamcorper faucibus velit, a semper neque interdum a. Vestibulum interdum nulla augue, ut bibendum lectus egestas sit amet. Aenean condimentum dolor egestas massa ullamcorper eleifend. In sagittis elit a quam convallis faucibus. Donec quam tortor, consequat vel quam efficitur, suscipit hendrerit dolor. Curabitur gravida venenatis commodo. In et tellus sed metus tincidunt semper. Donec sit amet luctus erat, a auctor massa.";
 
