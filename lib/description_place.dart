0c45fad8d93482b70ce0c09fb00d4f86e1087a51
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-02-10 13:48:46 -0300

    3er arreglo del texto de la descripción

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -24,16 +24,15 @@ class DescriptionPlace extends StatelessWidget {
     ]);
 
     final description = Container(
-      margin: EdgeInsets.only(top: 20.0, right: 20.0, left: 20.0),
+      margin: new EdgeInsets.only(top: 20.0, right: 20.0, left: 20.0),
       child: Text('''
           Lorem ipsum dolor sit amet,consectetuer adipiscin elit.Aenean commodo ligula eget dolor.Aenean massa.Cum sociis natoque penatibus et magnis dis parturient montes,nascetur ridiculus mus.Donec quam felis, ultricies nec,pellentesque eu.
           
           Pretium quis, sem. Nulia consequat massa quis enim.Donec pede justo, fringilla vel,aliquet nec, vulputate eget, arcu.In enim justo,rhoncus ut, imperdiet a.''',
-          style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w300,
-              color: Color(0xff54575a)),
-          textAlign: TextAlign.justify),
+          style: const TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF56575a))),
     );
 
     return Column(
