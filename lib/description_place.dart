947de54e305ff24845b59397c94f3cfd525b0f89
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-02-10 14:20:11 -0300

    Últimos arreglos en los archivos main y description_place a raiz de agregar el constructor

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -7,10 +7,23 @@ class DescriptionPlace extends StatelessWidget {
 
   DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
 
-  String descriptionDummy =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit nec elit ac placerat. Nam et sapien in justo posuere cursus. Maecenas sit amet pretium urna, a tincidunt mi. Morbi ac eros sed purus blandit tincidunt sodales in mi. Proin nec gravida ex. Donec iaculis dapibus nibh, ac tempus justo. Vivamus condimentum nisi in tellus efficitur, ac consequat metus lobortis. Duis ullamcorper faucibus velit, a semper neque interdum a. Vestibulum interdum nulla augue, ut bibendum lectus egestas sit amet. Aenean condimentum dolor egestas massa ullamcorper eleifend. In sagittis elit a quam convallis faucibus. Donec quam tortor, consequat vel quam efficitur, suscipit hendrerit dolor. Curabitur gravida venenatis commodo. In et tellus sed metus tincidunt semper. Donec sit amet luctus erat, a auctor massa.";
-
   Widget build(BuildContext context) {
+    final star_half = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
     final star = Container(
       margin: EdgeInsets.only(top: 323.0, right: 3.0),
       child: Icon(
@@ -22,19 +35,19 @@ class DescriptionPlace extends StatelessWidget {
       Container(
         margin: EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0),
         child: Text(
-          "Duwili Elle",
+          namePlace,
           style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
           textAlign: TextAlign.left,
         ),
       ),
       Row(
-        children: <Widget>[star, star, star, star, star],
+        children: <Widget>[star, star, star, star_half, star_border],
       ),
     ]);
 
     final description = Container(
       margin: new EdgeInsets.only(top: 20.0, right: 20.0, left: 20.0),
-      child: Text(descriptionDummy,
+      child: Text(descriptionPlace,
           style: const TextStyle(
               fontSize: 16.0,
               fontWeight: FontWeight.bold,
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  String descriptionDummy =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit nec elit ac placerat. Nam et sapien in justo posuere cursus. Maecenas sit amet pretium urna, a tincidunt mi. Morbi ac eros sed purus blandit tincidunt sodales in mi. Proin nec gravida ex. Donec iaculis dapibus nibh, ac tempus justo. Vivamus condimentum nisi in tellus efficitur, ac consequat metus lobortis. Duis ullamcorper faucibus velit, a semper neque interdum a. Vestibulum interdum nulla augue, ut bibendum lectus egestas sit amet. Aenean condimentum dolor egestas massa ullamcorper eleifend. In sagittis elit a quam convallis faucibus. Donec quam tortor, consequat vel quam efficitur, suscipit hendrerit dolor. Curabitur gravida venenatis commodo. In et tellus sed metus tincidunt semper. Donec sit amet luctus erat, a auctor massa.";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,7 +31,7 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(title: Text("Hola Mundo")),
-          body: new DescriptionPlace(),
+          body: new DescriptionPlace("Duwili Elle", 4, descriptionDummy),
         ) //MyHomePage(title: 'Flutter Demo Home Page'),
         );
   }
