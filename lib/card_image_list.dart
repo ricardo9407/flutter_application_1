b1bfea40ba1b0ce673e2a043b47a93355eb93715
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-02-11 15:32:17 -0300

    Implementación de la ListView

---
+++ lib/card_image_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'card_image.dart';
+
+class CardImageList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Container(
+      height: 350.0,
+      child: ListView(
+        padding: EdgeInsets.all(25.0),
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          CardImage("assets/image/beach.jpeg"),
+          CardImage("assets/image/beach_palm.jpeg"),
+          CardImage("assets/image/mountain_stars.jpeg"),
+          CardImage("assets/image/mountain.jpeg"),
+          CardImage("assets/image/river.jpeg"),
+          CardImage("assets/image/sunset.jpeg")
+        ],
+      ),
+    );
+  }
+}
