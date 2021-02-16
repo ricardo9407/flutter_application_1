a3fefd6b4cdafd856ce7fd24001e6b57341bd04d
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-02-16 13:18:06 -0300

    Implementación del Bottom Navegarion

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'platzi_trips.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: , 
-      );
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PlatziTrips(),
+    );
   }
 }
---
+++ lib/profile_trips.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileTrips extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    return Container(
+      color: Colors.indigo,
+    );
+  }
+}
